5243cb049052d3692e0aca93fdc1d4aaf86706f9
Andre Miranda <andre.miranda@snhu.edu>
2026-07-23 18:26:47 -0400

    Module 4 completed baseline

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const ctrlMain = require('../controllers/main');
 const ctrlTravel = require('../controllers/travel');
+const tripsController = require('../controllers/trips');
 
 // Static pages
 router.get('/', ctrlMain.index);
@@ -11,6 +12,10 @@ router.get('/contact', ctrlMain.contact);
 router.get('/meals', ctrlMain.meals);
 router.get('/news', ctrlMain.news);
 router.get('/rooms', ctrlMain.rooms);
+
+// Trips API endpoints
+router.get('/trips', tripsController.tripsList);
+router.get('/trips/:tripCode', tripsController.tripsFindByCode);
 
 // Dynamic pages
 router.get('/travel', ctrlTravel.travel);
